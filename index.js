7e620033af4ad7a945efa6a77bb8f6bfd4311024
Caio de Souza Silva <caio.silva@lojastorra.com.br>
2026-02-02 18:30:26 -0300

    fix: voltando versão do chalk

--- index.js
+++ index.js
@@ -21,8 +21,17 @@ function operation() {
     ])
     .then((answers) => {
       const action = answers["action"];
-      console.log(action);
+      if (action === "Criar conta") {
+        createAccount();
+      }
     })
 
     .catch((err) => console.log(err));
+}
+
+//create an account
+
+function createAccount() {
+  console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco! "));
+  console.log(chalk.green("Defina as opções da sua conta a seguir: "));
 }
