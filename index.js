91fcbb1b1afc5ac59330611e8bba356dc74c80b5
Caio de Souza Silva <caio.silva@lojastorra.com.br>
2026-02-02 18:24:39 -0300

    feat: incluindo lógica de contas

---
+++ index.js
@@ -0,0 +1,28 @@
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+const fs = require("fs");
+operation();
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer?",
+        choices: [
+          "Criar conta",
+          "Consultar saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answers) => {
+      const action = answers["action"];
+      console.log(action);
+    })
+
+    .catch((err) => console.log(err));
+}
